f417ad7736998c57a36a4d3bab6a061454ce0de4
DAVID SANTIAGO ORTEGA HEREDIA <87381799+dorteg5@users.noreply.github.com>
2025-02-22 10:00:57 -0500

    first commit

---
+++ src/main/java/com/kata/kata_cinema_mngr/KataCinemaMngrApplication.java
@@ -0,0 +1,13 @@
+package com.kata.kata_cinema_mngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCinemaMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataCinemaMngrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kata/kata_cinema_mngr/KataCinemaMngrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.kata_cinema_mngr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCinemaMngrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
